36e853a78d3094107a6eb8d5d1c5fcc9217c849a
olkaKorin <46497291+olkaKorin@users.noreply.github.com>
2019-03-01 15:01:01 +0200

    OK-4

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 // own
-import './styles/styles.scss'
-import List from './components/list'
-import Form from './components/form'
-import { list } from './consts'
-import Header from './components/header'
+import "./styles/styles.scss";
+import List from "./components/list";
+import Form from "./components/form";
+import { list } from "./consts";
+import Header from "./components/header";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = { list: null }
-    this.addItem = this.addItem.bind(this)
-    this.deleteItem = this.deleteItem.bind(this)
-    this.changeItemStatus = this.changeItemStatus.bind(this)
+    super(props);
+    this.state = { list: null };
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.changeItemStatus = this.changeItemStatus.bind(this);
   }
 
   componentDidMount() {
-    this.setState({ list })
+    this.setState({ list });
   }
 
   render() {
@@ -26,44 +26,44 @@ class App extends React.Component {
         <div className="app">
           <Header />
           <div className="app-list-form">
+            <Form addItem={this.addItem} />
             <List
               deleteItem={this.deleteItem}
               list={this.state.list}
               changeItemStatus={this.changeItemStatus}
             />
-            <Form addItem={this.addItem} />
           </div>
         </div>
-      )
+      );
     } else {
-      return <div>spinner</div>
+      return <div>spinner</div>;
     }
   }
 
   addItem(val) {
-    if (!val) return
-    const newItem = { id: Date.now(), title: val, done: false }
+    if (!val) return;
+    const newItem = { id: Date.now(), title: val, done: false };
     this.setState({
-      list: [...this.state.list, newItem],
-    })
+      list: [...this.state.list, newItem]
+    });
   }
 
   deleteItem(id) {
-    const filteredList = this.state.list.filter(item => item.id !== id)
+    const filteredList = this.state.list.filter(item => item.id !== id);
     this.setState({
-      list: filteredList,
-    })
+      list: filteredList
+    });
   }
 
   changeItemStatus(id) {
     const mappedList = this.state.list.map(item => {
-      if (item.id === id) item.done = !item.done
-      return item
-    })
+      if (item.id === id) item.done = !item.done;
+      return item;
+    });
     this.setState({
-      list: mappedList,
-    })
+      list: mappedList
+    });
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
